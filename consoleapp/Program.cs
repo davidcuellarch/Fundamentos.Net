7ca7db2c91f64860ebb9c7f91cf9fed75ef33722
David Cuellar <davidcuellarh@icloud.com>
2023-09-23 10:25:28 -0500

    Primer proyecto .net

---
+++ consoleapp/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ingrese su nombre");
+var userName = Console.ReadLine();
+
+Console.WriteLine($"Hola {userName}!");
+Console.WriteLine("Cual es su cargo?");
+var userPosition = Console.ReadLine();
+
+Console.WriteLine("Cual es su edad?");
+int userAge = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"Su nombre es {userName}, su cargo es {userPosition} y su edad es {userAge}");
+Console.WriteLine("Bienvenido a esta Empresa");
+Console.WriteLine("Presione cualquier tecla para salir");
+
+
